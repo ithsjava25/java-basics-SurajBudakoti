41eb39aadb7b59a8de1a6a88fa11da197642e22b
Suraj Budakoti <suraj.budakoti@iths.se>
2025-09-28 22:09:20 +0200

    Add code when --help is input as CLI

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,8 +2,27 @@ package com.example;
 
 import com.example.api.ElpriserAPI;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        ElpriserAPI elpriserAPI = new ElpriserAPI();
+        inputHelp(args[0]);
+
+        ElpriserAPI elpriserAPI = new ElpriserAPI(true);
+
+    }
+
+    public static void inputHelp(String help){
+        if (help.equals("--help")){
+            System.out.println(
+                    "--zone\n"+
+                    "SE1\n"+
+                    "SE2\n"+
+                    "SE3\n"+
+                    "SE4\n"+
+                    "--date\n"+
+                    "--sorted\n" +
+                    "--charging\n");
+        }
     }
 }
