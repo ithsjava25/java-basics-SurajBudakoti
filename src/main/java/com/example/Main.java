0003de41a517d01fbbb248cbb5920b5a6bb7811d
Suraj Budakoti <suraj.budakoti@iths.se>
2025-09-30 19:35:17 +0200

    Handle no arguments

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,39 +2,33 @@ package com.example;
 
 import com.example.api.ElpriserAPI;
 
+import java.lang.reflect.Array;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-        try{
-            if (!args[0].equals("--zone")){
-                System.out.println("\"zone\", \"required\"");
-                userInputHelp(args);
-            }
-        }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Please input something!");
-
-        }
-
-
+        checkNoArgumentsPresent(args);
+        checkMissingZoneArgument(args);
+        System.out.println(userInputHelp());
         ElpriserAPI elpriserAPI = new ElpriserAPI();
-        System.out.println(elpriserAPI.getPriser("2024-12-15", ElpriserAPI.Prisklass.SE1).get(2));
-//
     }
 
-    public static void userInputHelp(String[] help){
-        if (help[0].equals("--help") || help.length == 0) {
-            System.out.println(
-                    "--zone\n"+
-                    "SE1\n"+
-                    "SE2\n"+
-                    "SE3\n"+
-                    "SE4\n"+
-                    "--date\n"+
-                    "--sorted\n" +
-                    "--charging\n");
+    public static String userInputHelp() {
+        return ("--zone\n" + "SE1\n" + "SE2\n" + "SE3\n" + "SE4\n" + "--date\n" + "--sorted\n" + "--charging\n");
+    }
+
+    public static void checkNoArgumentsPresent(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Usage: " + userInputHelp());
+        }
+    }
+
+    public static void checkMissingZoneArgument(String[] args) {
+        if (!Arrays.asList(args).contains("zone")) {
+            System.out.println("\"zone\", \"required\"");
         }
     }
 }
