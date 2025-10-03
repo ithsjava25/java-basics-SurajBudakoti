ceea3bd606af478f05e31cf4090c930ada45a6a7
Suraj Budakoti <suraj.budakoti@iths.se>
2025-10-03 16:05:47 +0200

    Calculate hourly price from quarterly

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,13 +2,12 @@ package com.example;
 
 import com.example.api.ElpriserAPI;
 
-import java.lang.classfile.constantpool.DoubleEntry;
-import java.lang.reflect.Array;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.*;
 
 public class Main {
@@ -22,40 +21,84 @@ public class Main {
         ElpriserAPI elpriserAPI = new ElpriserAPI();
         ElpriserAPI.Prisklass prisklass = ElpriserAPI.Prisklass.SE2;
         LocalDate date = LocalDate.now();
-        date = LocalDate.of(2025, 9,4);
-        List<ElpriserAPI.Elpris> priser = new ArrayList<>(elpriserAPI.getPriser(date, prisklass));
-        List<String> startTid = new ArrayList<>();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH");
+        List<ElpriserAPI.Elpris> todayElPriser = new ArrayList<>(elpriserAPI.getPriser(date, prisklass));
 
-        //TODO: Fix format XX:XX
-       for (ElpriserAPI.Elpris tider: priser){
-           String formattedString = tider.timeStart().format(formatter);
-           startTid.add(formattedString + "-" + (Integer.parseInt(formattedString)+1));
-           }
-        System.out.println("Formatterad string " + startTid);
+//        TODO: Denna ska komma in om timme för LocalTime.now() >= 13
+//            Hämta dagens timma m.h.a: LocalTime.now().getHour())
+//        List<ElpriserAPI.Elpris> tomorrowElPriser = new ArrayList<>(elpriserAPI.getPriser(date.plusDays(1), prisklass));
 
-        List<Double> orePerkWh = new ArrayList<>();
-
-        //Formats sekPerkWh to orePerkWh and puts them into its own list.
-        for (ElpriserAPI.Elpris listOfElpriser: priser) {
-
-            //Converts to orePerKWh as string
-            Double convertSekToOre = listOfElpriser.sekPerKWh()*100;
-
-            //Adds to list called orePerKWh after converting to double
-            orePerkWh.add(convertSekToOre);
+        //Created a list for every hour.
+        List<Integer> hours = new ArrayList<>();
+        for (int i = 0; i < 24; i++) {
+            hours.add(i);
         }
-        System.out.println(orePerkWh);
+        for (ElpriserAPI.Elpris varjePris : todayElPriser){
+            System.out.println("Varje pris från API: " + varjePris.sekPerKWh());
+        }
+        //Created list for prices every hour
+        List<Double> hourlyPrices = new ArrayList<>();
+        double lowestPrice = 0;
+        double hourWhenLowestPrice = 0;
+        double highestPrice = 0;
+        double hourWhenHighestPrice = 0;
 
-       HashMap <String, Double> newElPriser = new HashMap<>();
-
-            for (int i = 0; i < priser.size(); i++) {
-                newElPriser.put(startTid.get(i), orePerkWh.get(i));
-                System.out.println(startTid.get(i) + " " + orePerkWh.get(i));
+        if (todayElPriser.size() == 96){
+            for (Integer everyHour: hours){
+            double sumOfHourForEveryQuarter = 0;
+                for (int i = 0; i < 4; i++) {
+                    sumOfHourForEveryQuarter += (todayElPriser.get(everyHour*4+i).sekPerKWh());
+                }
+                hourlyPrices.add(sumOfHourForEveryQuarter);
+            System.out.printf("sumOfHourForEveryQuarter: "+sumOfHourForEveryQuarter);
             }
-//      TODO: Sort newElPriser according to price
-        System.out.println(newElPriser);
+        }
+
+        List<String> betweenHours = new ArrayList<>();
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH");
+       for (ElpriserAPI.Elpris tider: todayElPriser){
+           String formattedTime = tider.timeStart().format(timeFormatter);
+
+           int addHour = (Integer.parseInt(formattedTime)+1);
+           String addHourToString = String.format("%02d", addHour);
+           if (addHour == 24){
+               addHourToString = "00";
+           }
+           betweenHours.add(formattedTime + "-" + addHourToString );
+       }
+
+//        List<Double> orePerkWh = new ArrayList<>();
+//
+//        //Formats sekPerkWh to orePerkWh and puts them into its own list.
+//        for (ElpriserAPI.Elpris listOfElpriser: todayElPriser) {
+//
+//            //Converts to orePerKWh as string
+//            Double convertedSekToOre = listOfElpriser.sekPerKWh()*100;
+//
+//            //Adds to list called orePerKWh after converting to double
+//            orePerkWh.add(convertedSekToOre);
+//        }
+//        System.out.println(orePerkWh);
+//
+//        if (orePerkWh.size() == 96){
+//            for (int i = 0; i < 24; i++) {
+//
+//
+//            }
+//        }
+//        System.out.println("Class : "  + orePerkWh.get(1).getClass());
+//      TODO: Add prices and times to HashMap newElPriser
+//       HashMap <String, Double> newElPriser = new HashMap<>();
+//            for (int i = 0; i < todayElPriser.size(); i++) {
+//                newElPriser.put(betweenHours.get(i), orePerkWh.get(i));
+//                System.out.println(betweenHours.get(i) + " " + orePerkWh.get(i));
+//            }
+//
+//      TODO: Method for sort newElPriser according to price
+//        System.out.println(newElPriser);
+
+//    TODO: Before printing out final prices, formatOre on prices to print out with "," instead of "."
     }
+
 
 
 //
@@ -68,30 +111,14 @@ public class Main {
 
 
 
-    public static void displayMinMaxPrice(List<ElpriserAPI.Elpris> priser){
-        priser.sort(Comparator.comparingDouble(pris->pris.sekPerKWh()));
-        double lowestPrice = priser.getFirst().sekPerKWh();
-        for (ElpriserAPI.Elpris lowest:priser){
-            if (lowest.sekPerKWh() < lowestPrice){
-                lowestPrice = lowest.sekPerKWh();
-            }
-        }
-
-        double highestPrice = priser.getFirst().sekPerKWh();
-        for (ElpriserAPI.Elpris highest:priser){
-            if (highest.sekPerKWh() > highestPrice){
-                highestPrice = highest.sekPerKWh();
-            }
-        }
-        double meanPrice = 0;
-        for (ElpriserAPI.Elpris mean: priser){
-            meanPrice += mean.sekPerKWh();
-        }
-
-        System.out.println("Lägsta pris: " + formatOre(lowestPrice));
-        System.out.println("Högsta pris: " + formatOre(highestPrice));
-        System.out.println("Medelpris : " + formatOre(meanPrice/priser.size()));
-    };
+//    TODO: Write method for calculating Min, Max and Mean price from Hashmap newElPriser
+//        Sort them and choose lowest/max number orePerKWh?
+//    public static void displayMinMaxPrice(){
+//
+//        System.out.println("Lägsta pris: " + formatToComma(lowestPrice));
+//        System.out.println("Högsta pris: " + formatToComma(highestPrice));
+//        System.out.println("Medelpris : " + formatToComma(meanPrice/priser.size()));
+//    };
 //    public static void checkIfUserInputHelp(String[] args){
 //            if (Arrays.asList(args).contains("--help")){
 //                userInputHelp();
@@ -121,11 +148,10 @@ public class Main {
 //    }
 //
 ////formatera om double till öre
-    private static String formatOre(double sekPerKWh) {
-        double ore = sekPerKWh * 100.0;
+    private static String formatToComma(double sekPerKWh) {
         DecimalFormatSymbols symbols = DecimalFormatSymbols.getInstance(Locale.of("sv", "SE"));
         DecimalFormat df = new DecimalFormat("0.00", symbols);
-        return df.format(ore);
+        return df.format(sekPerKWh);
     }
 }
 
